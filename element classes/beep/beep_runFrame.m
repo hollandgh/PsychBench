6a7ff4e8ae1367f1fd432ee423904271c917b7c2
hollandgh <65265772+hollandgh@users.noreply.github.com>
2024-08-04 17:05:26 -0400

    Delete element classes directory

--- element classes/beep/beep_runFrame.m
+++
@@ -1,4 +0,0 @@
-%MATLAB beep once when this script runs in object frame 0 (timing not important if using a beep object).
-%Then end object on its own.
-beep
-this = element_end(this);
--- element classes/rectangle/rectangle_close.m
+++
@@ -1,4 +0,0 @@
-if WITHPTB
-    %Close texture using PTB Close
-    Screen('Close', this.n_texture)
-end
